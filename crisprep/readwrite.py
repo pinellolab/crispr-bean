ab134c53e7b2cfebc7c4f7aea1ed454145e460be
Jayoung Ryu <jayoung_ryu@g.harvard.edu>
2021-11-11 19:15:26 -0500

    added lfc calculation

--- crisprep/readwrite.py
+++ crisprep/readwrite.py
@@ -66,5 +66,49 @@ class ReporterScreen(Screen):
         edits = edits.fillna(0)
         return(edits)
 
+    def log_norm(self):
+        super().log_norm()
+        super().log_norm(read_count_layer = "edits")
 
 
+    def log_fold_changes(self, cond1, cond2, return_result = False):
+        guide_fc = super().log_fold_change(cond1, cond2, return_result = return_result)
+        edit_fc = super().log_fold_change(cond1, cond2, lognorm_counts_key = "lognorm_edits", out_guides_suffix = "edit_lfc", return_result = return_result)
+        if return_result :
+            return((guide_fc, edit_fc))
+
+    def log_fold_change_aggregates(
+            self, 
+            cond1, 
+            cond2, 
+            aggregate_condit = "replicate", 
+            compare_condit = "sort", 
+            aggregate_fn = "median", 
+            return_result = False, 
+            keep_per_replicate = False):
+        guide_fc_agg = super().log_fold_change_aggregate(
+                cond1, 
+                cond2, 
+                lognorm_counts_key = "lognorm_counts", 
+                aggregate_condit = aggregate_condit,
+                compare_condit = compare_condit,
+                out_guides_suffix = "lfc", 
+                aggregate_fn = aggregate_fn,
+                return_result = return_result,
+                keep_per_replicate = keep_per_replicate
+                )
+
+        edit_fc_agg = super().log_fold_change_aggregate(
+                cond1, 
+                cond2, 
+                lognorm_counts_key = "lognorm_edits", 
+                aggregate_condit = aggregate_condit,
+                compare_condit = compare_condit,
+                out_guides_suffix = "edit_lfc", 
+                aggregate_fn = aggregate_fn,
+                return_result = return_result,
+                keep_per_replicate = keep_per_replicate
+                )
+
+        if return_result:
+            return((guide_fc_agg, edit_fc_agg))

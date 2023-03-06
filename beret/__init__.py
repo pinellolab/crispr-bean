f500cc86a582c43e4b63bcc36aa92ba0d18e4b63
Jayoung Ryu <jayoung_ryu@g.harvard.edu>
2023-03-06 18:15:50 -0500

    add untracked modules

--- beret/__init__.py
+++ beret/__init__.py
@@ -2,7 +2,13 @@ from .framework.Edit import Edit, Allele
 from .framework.filter_alleles import filter_alleles
 from .framework.ReporterScreen import ReporterScreen, concat, read_h5ad
 from . import mapping as mp
+from . import plotting as pl
 from . import annotate as an
 from . import framework as fr
-from .framework.AminoAcidEdit import AminoAcidEdit, AminoAcidAllele, CodingNoncodingAllele
+from . import qc as qc
+from .framework.AminoAcidEdit import (
+    AminoAcidEdit,
+    AminoAcidAllele,
+    CodingNoncodingAllele,
+)
 from .annotate.translate_allele import translate_allele, translate_allele_df

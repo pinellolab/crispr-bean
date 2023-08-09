72e738be379fff361173821287c0d3661bff0263
Jayoung Ryu <jayoung_ryu@g.harvard.edu>
2023-08-09 14:48:41 -0400

    fix guide self-edit allele quantification

--- tests/test_count.py
+++ tests/test_count.py
@@ -6,6 +6,11 @@ def test_count():
         "bean-count --R1 data/test_R1.fastq --R2 data/test_R2.fastq -b A -f data/test_guide_info.csv -o test_res/ -r"
     )
 
+def test_guide_count():
+    os.system(
+         "bean-count --R1 data/test_R1.fastq --R2 data/test_R2.fastq -b A -f data/test_guide_info.csv -o test_res/ -g"
+            )
+
 def test_count_samples():
     os.system(
         "python ../../bin/bean-count-samples --input data/sample_list.csv -b A -f data/test_guide_info.csv -o test_res/ -r --guide-start-seq=GGAAAGGACGAAACACCG"

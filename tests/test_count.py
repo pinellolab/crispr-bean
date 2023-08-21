b0fec10ac11cbff3940277b8d8e2cc70b1d79416
Jayoung Ryu <jayoung_ryu@g.harvard.edu>
2023-08-21 11:52:14 -0400

    use requirements instead

--- tests/test_count.py
+++ tests/test_count.py
@@ -1,17 +1,37 @@
-import os
+import subprocess
 
 
 def test_count():
-    os.system(
-        "bean-count --R1 data/test_R1.fastq --R2 data/test_R2.fastq -b A -f data/test_guide_info.csv -o test_res/ -r"
-    )
+    cmd = "bean-count --R1 tests/data/test_R1.fastq --R2 tests/data/test_R2.fastq -b A -f tests/data/test_guide_info.csv -o tests/test_res/ -r --guide-start-seq=GGAAAGGACGAAACACCG"
+    try:
+        subprocess.check_output(
+            cmd,
+            shell=True,
+            universal_newlines=True,
+        )
+    except subprocess.CalledProcessError as exc:
+        raise exc
+
 
 def test_guide_count():
-    os.system(
-         "bean-count --R1 data/test_R1.fastq --R2 data/test_R2.fastq -b A -f data/test_guide_info.csv -o test_res/ -g"
-            )
+    cmd = "bean-count --R1 data/test_R1.fastq --R2 data/test_R2.fastq -b A -f data/test_guide_info.csv -o test_res/ -g --guide-start-seq=GGAAAGGACGAAACACCG"
+    try:
+        subprocess.check_output(
+            cmd,
+            shell=True,
+            universal_newlines=True,
+        )
+    except subprocess.CalledProcessError as exc:
+        raise exc
+
 
 def test_count_samples():
-    os.system(
-        "python ../../bin/bean-count-samples --input data/sample_list.csv -b A -f data/test_guide_info.csv -o test_res/ -r --guide-start-seq=GGAAAGGACGAAACACCG"
-    )
+    cmd = "python ../../bin/bean-count-samples --input data/sample_list.csv -b A -f data/test_guide_info.csv -o test_res/ -r --guide-start-seq=GGAAAGGACGAAACACCG"
+    try:
+        subprocess.check_output(
+            cmd,
+            shell=True,
+            universal_newlines=True,
+        )
+    except subprocess.CalledProcessError as exc:
+        raise exc

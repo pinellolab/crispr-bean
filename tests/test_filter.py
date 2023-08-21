9abf8d33c45792ba711c18d793446e6a8cefe5b5
Jayoung Ryu <jayoung_ryu@g.harvard.edu>
2023-08-21 16:08:39 -0400

    adding filters

--- tests/test_filter.py
+++ tests/test_filter.py
@@ -1,6 +1,25 @@
-import os
+import subprocess
 
-def test_filter():
-    os.system(
-        "bean-filter data/bean_count_sample_list.h5ad -o tmp -s 0 -e 19 -w -b -t -ap 0.1 -sp 0.3"
-    )
+
+def test_filter_varscreen():
+    cmd = "bean-filter tests/data/var_mini_screen.h5ad -o tmp -s 0 -e 19 -w -b -t -ap 0.1 -sp 0.3"
+    try:
+        subprocess.check_output(
+            cmd,
+            shell=True,
+            universal_newlines=True,
+        )
+    except subprocess.CalledProcessError as exc:
+        raise exc
+
+
+def test_filter_tiling_screen():
+    cmd = "bean-filter tests/data/tiling_mini_screen.h5ad -o tmp -s 0 -e 19 -w -b -t -ap 0.1 -sp 0.3"
+    try:
+        subprocess.check_output(
+            cmd,
+            shell=True,
+            universal_newlines=True,
+        )
+    except subprocess.CalledProcessError as exc:
+        raise exc

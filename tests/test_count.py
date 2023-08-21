70cb28bc07d6e1a15183011c6c676dabce72a10e
Jayoung Ryu <jayoung_ryu@g.harvard.edu>
2023-08-21 12:11:09 -0400

    chancing order of python and conda

--- tests/test_count.py
+++ tests/test_count.py
@@ -14,7 +14,7 @@ def test_count():
 
 
 def test_guide_count():
-    cmd = "bean-count --R1 data/test_R1.fastq --R2 data/test_R2.fastq -b A -f data/test_guide_info.csv -o test_res/ -g --guide-start-seq=GGAAAGGACGAAACACCG"
+    cmd = "bean-count --R1 tests/data/test_R1.fastq --R2 tests/data/test_R2.fastq -b A -f tests/data/test_guide_info.csv -o tests/test_res/ -g --guide-start-seq=GGAAAGGACGAAACACCG"
     try:
         subprocess.check_output(
             cmd,
@@ -26,7 +26,7 @@ def test_guide_count():
 
 
 def test_count_samples():
-    cmd = "python ../../bin/bean-count-samples --input data/sample_list.csv -b A -f data/test_guide_info.csv -o test_res/ -r --guide-start-seq=GGAAAGGACGAAACACCG"
+    cmd = "bean-count-samples --input tests/data/sample_list.csv -b A -f tests/data/test_guide_info.csv -o tests/test_res/ -r --guide-start-seq=GGAAAGGACGAAACACCG"
     try:
         subprocess.check_output(
             cmd,

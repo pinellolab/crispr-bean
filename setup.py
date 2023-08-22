87877321a32c48abbb1c989cfe4b64d7299d18f6
Jayoung Ryu <jayoung_ryu@g.harvard.edu>
2023-08-22 01:22:41 -0400

    update perturb-tools dependency

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ setup(
     ],
     install_requires=[
         "numpy",
-        "perturb-tools>=0.2.3",
+        "perturb-tools>=0.2.5",
         "anndata>=0.8.0",
         "Bio>=1.5",
         "matplotlib",

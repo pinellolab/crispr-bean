f52937ccd9942dd021da47897231a758522da53f
jykr <jayoung_ryu@g.harvard.edu>
2024-03-30 14:29:16 -0400

    Fix QC notebook

--- tests/test_qc.py
+++ tests/test_qc.py
@@ -17,7 +17,7 @@ def test_qc():
 
 @pytest.mark.order(8)
 def test_qc_tiling():
-    cmd = "bean qc tests/data/tiling_mini_screen.h5ad -o tests/data/tiling_mini_screen_masked.h5ad -r tests/test_res/qc_report_tiling_mini_screen --count-correlation-thres 0.6  --posctrl-col ''"
+    cmd = "bean qc tests/data/tiling_mini_screen.h5ad -o tests/data/tiling_mini_screen_masked.h5ad -r tests/test_res/qc_report_tiling_mini_screen --count-correlation-thres 0.6  "
     try:
         subprocess.check_output(
             cmd,
@@ -56,12 +56,11 @@ def test_dummy_insertion_varscreen():
 
 @pytest.mark.order(10)
 def test_dummy_insertion_tilingscreen():
-    cmd = "bean qc tests/data/tiling_mini_screen_missing.h5ad -o tests/data/tiling_mini_screen_missing_masked.h5ad -r tests/test_res/qc_report_tiling_mini_screen_missing --count-correlation-thres 0.6  --posctrl-col -b''"
+    cmd = "bean qc tests/data/tiling_mini_screen_missing.h5ad -o tests/data/tiling_mini_screen_missing_masked.h5ad -r tests/test_res/qc_report_tiling_mini_screen_missing --count-correlation-thres 0.6  -b"
     try:
         subprocess.check_output(
             cmd, shell=True, universal_newlines=True, stderr=subprocess.STDOUT
         )
         raise ValueError("Filtering should fail with too small number of replicates.")
     except subprocess.CalledProcessError as exc:
-        if "Too small number of replicate left after QC" not in exc.output:
-            raise exc
+        raise exc

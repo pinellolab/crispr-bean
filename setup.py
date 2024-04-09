ecc740ff15b6372a59853abbc76b7d37b8716ead
jykr <jayoung_ryu@g.harvard.edu>
2024-04-09 12:08:08 -0400

    manual release

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="crispr-bean",
-    version="{{VERSION_PLACEHOLDER}}",
+    version="1.1.0",
     python_requires=">=3.8.0",
     author="Jayoung Ryu",
     author_email="jayoung_ryu@g.harvard.edu",

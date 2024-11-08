3fdfb5d23ae3ea4d154f48c29087cc6915a644a0
Jayoung Ryu <jayoung_ryu@g.harvard.edu>
2024-11-08 18:04:01 +0000

    add the correct test cases for duplicated guide mapping

--- tests/test_count.py
+++ tests/test_count.py
@@ -46,7 +46,7 @@ def test_count_samples_match():
 
 @pytest.mark.order(105)
 def test_count_samples_nofilter():
-    cmd = "bean count-samples -i tests/data/sample_list.csv -b A -f tests/data/test_guide_info.csv -o tests/test_res/var/ -r --guide-start-seq=GGAAAGGACGAAACACCG --skip-filtering"
+    cmd = "bean count-samples -i tests/data/sample_list.csv -b A -f tests/data/test_guide_info.csv -o tests/test_res/var/ -r --guide-start-seq=GGAAAGGACGAAACACCG --skip-filtering --rerun"
     try:
         subprocess.check_output(
             cmd,
@@ -58,6 +58,19 @@ def test_count_samples_nofilter():
 
 
 @pytest.mark.order(106)
+def test_count_samples_duplicated():
+    cmd = "bean count-samples -i tests/data/sample_list.csv -b A -f tests/data/test_guide_info.csv -o tests/test_res/var/ -r --guide-start-seq=GGAAAGGACGAAACACCG --rerun --map-duplicated-to-best"
+    try:
+        subprocess.check_output(
+            cmd,
+            shell=True,
+            universal_newlines=True,
+        )
+    except subprocess.CalledProcessError as exc:
+        raise exc
+
+
+@pytest.mark.order(107)
 def test_count_samples_dual():
     cmd = "bean count-samples -i tests/data/sample_list.csv -b A,C -f tests/data/test_guide_info.csv -o tests/test_res/var/ -r --guide-start-seq=GGAAAGGACGAAACACCG"
     try:
@@ -70,9 +83,9 @@ def test_count_samples_dual():
         raise exc
 
 
-@pytest.mark.order(107)
+@pytest.mark.order(108)
 def test_count_samples_bcstart():
-    cmd = "bean count-samples -i tests/data/sample_list.csv -b A -f tests/data/test_guide_info.csv -o tests/test_res/var2/ -r --barcode-start-seq=GGAA --map-duplicated-to-best"
+    cmd = "bean count-samples -i tests/data/sample_list.csv -b A -f tests/data/test_guide_info.csv -o tests/test_res/var2/ -r --barcode-start-seq=GGAA --skip-filtering --rerun "
     try:
         subprocess.check_output(
             cmd,
@@ -108,7 +121,7 @@ def test_barcode_start_idx():
     assert bc == "AGAA"
 
 
-@pytest.mark.order(108)
+@pytest.mark.order(109)
 def test_count_samples_tiling():
     cmd = "bean count-samples -i tests/data/sample_list_tiling.csv -b A -f tests/data/test_guide_info_tiling_chrom.csv -o tests/test_res/tiling/ -r --tiling"
     try:
@@ -121,7 +134,7 @@ def test_count_samples_tiling():
         raise exc
 
 
-@pytest.mark.order(109)
+@pytest.mark.order(110)
 def test_count_chroms():
     cmd = "bean count --R1 tests/data/test_tiling_R1.fastq --R2 tests/data/test_tiling_R2.fastq -b A -f tests/data/test_guide_info_tiling_chrom.csv -o tests/test_res/tiling_chrom/ -r --tiling"
     try:
